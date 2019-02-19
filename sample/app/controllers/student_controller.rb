e04a3edc483b5e577631a5d98d9dea5993c04ded
dharmik897 <dharmiksalakiya8.ds@gmail.com>
2019-02-19 13:02:26 +0530

    changes in crud

--- sample/app/controllers/student_controller.rb
+++ sample/app/controllers/student_controller.rb
@@ -1,44 +1,30 @@
 class StudentController < ApplicationController
+  before_action :find_record, only: %i[edit show update destroy]
   def index
     @students = Student.all
   end
-
-  def show
-    @student = Student.find(params[:id])
-  end
-
+  def show; end
   def new
     @student = Student.new
   end
-
-  def edit
-    @student= Student.find(params[:id])
-  end
-  
+  def edit; end
   def create
     @student = Student.new(student_params)
-  
     if @student.save
-        redirect_to student_index_path
-      else
-        render 'new'
+      redirect_to student_index_path
+    else
+      render 'new'
     end
   end
-
   def update
-    @student = Student.find(params[:id])
-   
     if @student.update(student_params)
       redirect_to student_index_path
     else
       render 'edit'
     end
   end
-  
   def destroy
-    @student = Student.find(params[:id])
     @student.destroy
-     
     redirect_to student_index_path
   end
   
@@ -47,4 +33,8 @@ class StudentController < ApplicationController
     def student_params
       params.require(:student).permit(:name, :email, :phone)
     end
+    def find_record
+      @student= Student.find_by(id: params[:id])
+    end
+
 end
